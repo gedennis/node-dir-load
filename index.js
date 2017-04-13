9f721a572821e7ea8164d347655d6bdd1653370b
dennis.ge <yongyuanqianrushi@163.com>
2017-04-13 22:46:32 +0800

    feat(filter): use filter option to filter files

--- index.js
+++ index.js
@@ -7,14 +7,19 @@ const isRegExp = require('lodash/isRegExp');
 const fromFile = module.parent.filename;
 const baseDir = path.dirname(fromFile);
 
+const allowExts = ['.js', '.json'];
+
 const load = (payload = {}) => {
   const opt = defaults(payload, {
     dirname: baseDir,
     excludeDirs: /^\./,
+    filter: /^([^.]+).js(on)?$/,
     recursive: true,
   });
 
-  if (opt.excludeDirs && !isRegExp(opt.excludeDirs)) {
+  if (opt.excludeDirs
+    && !isRegExp(opt.excludeDirs)
+    && !isRegExp(opt.filter)) {
     throw new Error('The option excludeDirs should be an RegExp object.');
   }
 
@@ -26,6 +31,9 @@ const load = (payload = {}) => {
   const isExcludeDirectory = file =>
     (!opt.recursive || (opt.excludeDirs && opt.excludeDirs.test(file)));
 
+  const isExcludeFile = (file, ext) =>
+    !opt.filter.test(file) || !allowExts.includes(ext);
+
   fs.readdirSync(opt.dirname).forEach((file) => {
     const filePath = path.join(opt.dirname, file);
     if (fs.statSync(filePath).isDirectory()) {
@@ -33,10 +41,14 @@ const load = (payload = {}) => {
 
       modules[file] = load({
         dirname: filePath,
+        excludeDirs: opt.excludeDirs,
+        filter: opt.filter,
         recursive: opt.recursive,
       });
     } else {
-      const filename = path.basename(filePath, '.js');
+      const ext = path.extname(filePath);
+      if (isExcludeFile(file, ext)) return;
+      const filename = path.basename(filePath, ext);
       modules[filename] = module.require(filePath);
     }
   });
