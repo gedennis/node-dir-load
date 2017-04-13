20b5c401979e59a6250e25272a13e8af1bbe6e8e
dennis.ge <yongyuanqianrushi@163.com>
2017-04-13 21:36:38 +0800

    feat(exclude dir): add excludeDirs option to exclude dirs to load

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 
 const path = require('path');
 const defaults = require('lodash/defaults');
+const isRegExp = require('lodash/isRegExp');
 
 const fromFile = module.parent.filename;
 const baseDir = path.dirname(fromFile);
@@ -9,15 +10,21 @@ const baseDir = path.dirname(fromFile);
 const load = (payload = {}) => {
   const opt = defaults(payload, {
     dirname: baseDir,
+    excludeDirs: /^\./,
     recursive: true,
   });
+
+  if (opt.excludeDirs && !isRegExp(opt.excludeDirs)) {
+    throw new Error('The option excludeDirs should be an RegExp object.');
+  }
 
   const modules = {};
   if (!path.isAbsolute(opt.dirname)) {
     opt.dirname = path.resolve(baseDir, opt.dirname);
   }
 
-  const isExcludeDirectory = () => !opt.recursive;
+  const isExcludeDirectory = file =>
+    (!opt.recursive || (opt.excludeDirs && opt.excludeDirs.test(file)));
 
   fs.readdirSync(opt.dirname).forEach((file) => {
     const filePath = path.join(opt.dirname, file);
